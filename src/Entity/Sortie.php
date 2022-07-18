d392aac0afa104415ae46beb9c55975f10aa3d2e
RobinHillmeyer <robin.hillmeyer13@gmail.com>
2022-07-18 14:16:51 +0200

    Create Etat + Sortie

---
+++ src/Entity/Sortie.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SortieRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SortieRepository::class)]
+class Sortie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column()]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $startDateTime = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $duration = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $registrationDeadLine = null;
+
+    #[ORM\Column]
+    private ?int $registrationNumberMax = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $information = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getStartDateTime(): ?\DateTimeInterface
+    {
+        return $this->startDateTime;
+    }
+
+    public function setStartDateTime(\DateTimeInterface $startDateTime): self
+    {
+        $this->startDateTime = $startDateTime;
+
+        return $this;
+    }
+
+    public function getDuration(): ?\DateTimeInterface
+    {
+        return $this->duration;
+    }
+
+    public function setDuration(\DateTimeInterface $duration): self
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    public function getRegistrationDeadLine(): ?\DateTimeInterface
+    {
+        return $this->registrationDeadLine;
+    }
+
+    public function setRegistrationDeadLine(\DateTimeInterface $registrationDeadLine): self
+    {
+        $this->registrationDeadLine = $registrationDeadLine;
+
+        return $this;
+    }
+
+    public function getRegistrationNumberMax(): ?int
+    {
+        return $this->registrationNumberMax;
+    }
+
+    public function setRegistrationNumberMax(int $registrationNumberMax): self
+    {
+        $this->registrationNumberMax = $registrationNumberMax;
+
+        return $this;
+    }
+
+    public function getInformation(): ?string
+    {
+        return $this->information;
+    }
+
+    public function setInformation(?string $information): self
+    {
+        $this->information = $information;
+
+        return $this;
+    }
+}
