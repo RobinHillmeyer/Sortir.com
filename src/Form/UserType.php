1d537ea5366326e4e05cd346a36d80975ea4913b
ldorle <lucasdorle@hotmail.fr>
2022-07-19 13:20:25 +0200

    add password confirmation

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Faker\Provider\Text;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -31,8 +32,10 @@ class UserType extends AbstractType
             ->add('email', TextType::class, [
                 'label' => 'Email : '
             ])
-            ->add('password', PasswordType::class, [
-                'label' => 'Mot de passe : '
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => ['label' => 'Mot de passe : '],
+                'second_options' => ['label' => 'Confirmez votre mot de passe : ']
             ])
         ;
     }
