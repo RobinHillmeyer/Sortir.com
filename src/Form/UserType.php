4f71e8d280fdfc43526e63bb1efab98fdf7492ab
ldorle <lucasdorle@hotmail.fr>
2022-07-21 08:55:37 +0200

    git ignore img

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -13,7 +13,6 @@ use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 class UserType extends AbstractType
 {
