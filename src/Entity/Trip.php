efadf67c1c73f396e6dd98415cf825aea50a7ccd
ldorle <lucasdorle@hotmail.fr>
2022-07-24 18:26:51 +0200

    add TODO

--- src/Entity/Trip.php
+++ src/Entity/Trip.php
@@ -34,6 +34,8 @@ class Trip
     #[ORM\Column]
     private ?int $registrationNumberMax = null;
 
+    // TODO : mettre le nombre de personnes inscrites en cours
+
     #[ORM\Column(type: Types::TEXT, nullable: true)]
     private ?string $information = null;
 
